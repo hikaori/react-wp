6a4ff84c8514e327064f649c41ae25f54f0b8dcf
hikaori <kaori55194@gmail.com>
2019-06-14 17:32:58 -0700

    add video loop

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { BasicLayout } from '../';
+import ReactPlayer from 'react-player';
+
 import hero from '../../assets/images/COS-WebsiteHomePageHeroVideoOptimized.mp4';
 
 class LandingPage extends Component {
   render() {
     return (
       <BasicLayout>
-        <div>
-          <video
-            src={hero}
-            style={{
-              maxWidth: `100%`,
-            }}
-            autoPlay
-            muted
+        <div style={{ position: 'relative' }}>
+          <ReactPlayer
+            url="https://cosvancouver.com/wp-content/uploads/2019/06/COS-WebsiteHomePageHeroVideoOptimized.mp4"
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="100%"
           />
-          <div>あなたにとっての、海外への架け橋として</div>
+          <div style={{ position: 'absolute', top: '50%', right: '25%' }}>
+            あなたにとっての、海外への架け橋として
+          </div>
+        </div>
+        <div className="">
+          <img />
+          <div>
+            『留学』というのは手段であり、必ず目的が伴います。
+            世界中に友達を作りたい。将来武器になる高い英語力、専門知識をつけて日本でキャリアアップ。海外に生活の拠点を移す。
+            皆様の目的は何でしょうか。
+            COSでは目的ごとに大きく異なる専門知識の提供、サポートを確実に行えるように、保育留学・就職、クリエイター留学・就職、高校留学を別々のサービスとして運営しています。
+          </div>
         </div>
       </BasicLayout>
     );
